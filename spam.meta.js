277cb3e571acbc1f9b2225602d058cba04d504e4
user7215 <user7215@proton.me>
2025-08-18 10:26:18 -0700

    Create spam.meta.js

---
+++ spam.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Flag as Spam Button for SE
+// @namespace    https://user7215.github.io/
+// @version      2025-08-18
+// @description  try to take over the world!
+// @author       user7215
+// @match        https://github.com/user7215/flag-spam-se/blob/main/spam.user.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
