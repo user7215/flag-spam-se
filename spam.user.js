1ff499d3dd2061d3adc919bc5111f55aa68e4aa2
user7215 <user7215@proton.me>
2025-08-21 19:40:24 -0700

    Update spam.user.js

--- spam.user.js
+++ spam.user.js
@@ -19,6 +19,8 @@
 
 var qid = location.pathname.split("/")[2]
 var site = location.hostname;
+var token = localStorage.getItem("user7215-flagspam-accessToken");
+var key = "rl_jhoXv9U3Hh3swDc3aioi1uWKo";
 
 var div = document.createElement("div");
 div.className = "flex--item";
@@ -27,18 +29,30 @@ var btn = document.createElement("button");
 btn.innerText = "Spam";
 btn.className = "s-btn s-btn__link"; 
 btn.onclick = function(){
-    if (!localStorage.getItem("user7215-flagspam-accessToken")){ // get access token if not exist.
+    if (!token)){ // get access token if not exist.
       alert("Redirecting to get API access token. This question has not been flagged as spam. This will happen one time for every separate domain and every time you clear your localStorage (cookies).")
       location.href = "https://user7215.github.io/flag-spam-se/access-token.html?redirect=" + location.href;
     }
     // Flag spam
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "//api.stackexchange.com/2.3/questions/" + qid + "/flags/options?key=rl_jhoXv9U3Hh3swDc3aioi1uWKo&site=meta&access_token=" + localStorage.getItem("user7215-flagspam-accessToken"), false);
+    xhr.open("GET", "//api.stackexchange.com/2.3/questions/" + qid + "/flags/options?key=" + key + "&site=" + site + "&access_token=" + token, false);
     xhr.send();
     optionsJSON = JSON.parse(xhr.responseText);
     console.log(optionsJSON.toString())
     var optionID = optionsJSON.items[0].option_id;
-    console.log(optionID)
+    console.log(optionID);
+    xhr.open("POST", "https://api.stackexchange.com/2.3/questions/" + qid + "/flags/add/", false);
+    xhr.setRequestHeader
+    var formData = new FormData();
+    formData.appemd("id", qid);
+    formData.appemd("option_id", optionID);
+    formData.appemd("comment", "Done through user7215's flag-spam-se script. https://github.com/user7215/flag-spam-se");
+    formData.appemd("key", key);
+    formData.appemd("access_token", token);
+    formData.appemd("preview", true); // testing right now
+    formData.appemd("filter", default);
+    formData.appemd("site", site);
+    xhr.send(formData);
 };
 
 if (location.hash.includes("accessToken")){ // save access token
