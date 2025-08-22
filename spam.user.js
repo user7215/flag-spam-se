c9946be847938c2a15f8d267d1c536379b98a4e9
user7215 <user7215@proton.me>
2025-08-21 20:26:26 -0700

    Update spam.user.js

--- spam.user.js
+++ spam.user.js
@@ -20,6 +20,7 @@
 
 // -----CONFIG-----
 var testing_mode = false; // when testing set to true
+var error_message = "There was an error flagging the question as spam. This might occur because this is your own question or you have already flagged this question/retracted the flag. If you think this is a bug, check the console for details.";
 // -----END CONFIG -----
 
 // ---- API CONFIG ---
@@ -28,6 +29,8 @@ var site = location.hostname;
 var token = localStorage.getItem("user7215-flagspam-accessToken");
 var key = "rl_jhoXv9U3Hh3swDc3aioi1uWKo";
 // --- END API CONFIG ---
+
+// create span button
 
 var div = document.createElement("div");
 div.className = "flex--item";
@@ -48,29 +51,34 @@ btn.onclick = function(){
     var optionsJSON = JSON.parse(options);
     var optionID = optionsJSON.items[0].option_id;
     if (!optionID){
-        alert("There was an error flagging the question as spam. This might occur because this is your own question or you have already flagged this question. If you think this is a bug, check the console for details.")
-        console.log("There was an error flagging the question as spam. This might occur because this is your own question or you have already flagged this question. If you think this is a bug, create an issue on GitHub (https://github.com/user7215/flag-spam-se) or ask on the StackApps post (https://stackapps.com/questions/11876/placeholder-flag-as-spam-quick-button). Please include the below debug output. Please include the below")
+        alert(error_message)
+        console.log("There was an error flagging the question as spam. This might occur because this is your own question or you have already flagged this question. If you think this is a bug, create an issue on GitHub (https://github.com/user7215/flag-spam-se) or ask on the StackApps post (https://stackapps.com/questions/11876/placeholder-flag-as-spam-quick-button). Please include the below debug output. Please include the below output.");
         console.log("options: ", options, "\noptionsJSON: ", optionsJSON, "\noptionID: ", optionID);
     } else {
         xhr.open("POST", "https://api.stackexchange.com/2.3/questions/" + qid + "/flags/add/", false);
-        xhr.setRequestHeader
         var formData = new FormData();
         formData.append("id", qid);
         formData.append("option_id", optionID);
-        formData.append("comment", "Done through user7215's flag-spam-se script. https://github.com/user7215/flag-spam-se");
+        formData.append("comment", "Done through user7215's Flag as Spam Quick Button userscript. https://github.com/user7215/flag-spam-se https://stackapps.com/questions/11876/flag-as-spam-quick-button");
         formData.append("key", key);
         formData.append("access_token", token);
         formData.append("preview", testing_mode);
         formData.append("filter", "default");
         formData.append("site", site);
         xhr.send(formData);
+        if (xhr.status !== 200){
+            alert(error_message);
+            console.log("There was an error flagging the question as spam. This might occur because this is your own question or you have already flagged this question. If you think this is a bug, create an issue on GitHub (https://github.com/user7215/flag-spam-se) or ask on the StackApps post (https://stackapps.com/questions/11876/placeholder-flag-as-spam-quick-button). Please include the below debug output. Please include the below output.");
+            console.log("/questions/{id}/flags/add returned " + xhr.status + " instead of 200. responseText: " + xhr.responseText);
+        }
     }
 };
 
 if (location.hash.includes("accessToken")){ // save access token
   let accessToken = location.hash.split("#")[1].split("=")[1];
-  localStorage.setItem("user7215-flagspam-accessToken", accessToken)
+  localStorage.setItem("user7215-flagspam-accessToken", accessToken);
 }
 
+// append spam button
 div.appendChild(btn);
 document.getElementsByClassName("d-flex gs8 s-anchors s-anchors__muted fw-wrap")[0].appendChild(div);
